44b01a58533db48e2e09ae05ea03abae6124f92a
0xmt <markus.08xmt@gmail.com>
2024-01-24 11:46:11 +0100

    Add claimed amount to claim event (#11)

--- src/DolaSavings.sol
+++ src/DolaSavings.sol
@@ -176,9 +176,10 @@ contract DolaSavings {
      * @param to The address to receive the claimed DBR tokens.
      */
     function claim(address to) external updateIndex(msg.sender) {
-        dbr.mint(to, accruedRewards[msg.sender]);
+        uint accrued = accruedRewards[msg.sender];
+        dbr.mint(to, accrued);
         accruedRewards[msg.sender] = 0;
-        emit Claim(msg.sender, to);
+        emit Claim(msg.sender, to, accrued);
     }
 
     /**
@@ -197,7 +198,7 @@ contract DolaSavings {
 
     event Stake(address indexed caller, address indexed recipient, uint amount);
     event Unstake(address indexed caller, uint amount);
-    event Claim(address indexed caller, address indexed recipient);
+    event Claim(address indexed caller, address indexed recipient, uint claimed);
 
     event SetYearlyRewardBudget(uint newYearlyRewardBudget);
     event SetMaxRewardPerDolaMantissa(uint newMax);
